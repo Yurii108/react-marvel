0fdfbaf04e3cca5a7aa8845bb5fb79dcb02af1a8
Yurii <paraska108@gmail.com>
2022-09-12 17:47:24 +0300

    get information about character which we cliked

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -81,37 +81,43 @@ class CharInfo extends Component {
 
 const View = ({ char }) => {
     const { name, thumbnail, description, homepage, wiki, comics } = char;
-    <>
-        <div className="char__basics">
-            <img src={thumbnail} alt={name} />
-            <div>
-                <div className="char__info-name">{name}</div>
-                <div className="char__btns">
-                    <a href={homepage} className="button button__main">
-                        <div className="inner">homepage</div>
-                    </a>
-                    <a href={wiki} className="button button__secondary">
-                        <div className="inner">Wiki</div>
-                    </a>
+    const http = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
+    let objectFit = thumbnail === http ? { 'objectFit': 'fill' } : null;
+    return (
+        <>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={objectFit} />
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="char__descr">
-            {description}
-        </div>
-        <div className="char__comics">Comics:</div>
-        <ul className="char__comics-list">
-            {
-                comics.map((item, i) => {
-                    return (
-                        <li key={i} className="char__comics-item">
-                            {item.name}
-                        </li>
-                    )
-                })
-            }
-        </ul>
-    </>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comics.length > 0 ? null : 'There is no comics with this character'}
+                {
+                    comics.map((item, i) => {
+                        if (i > 9) return;
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </>
+    )
 }
 
 
