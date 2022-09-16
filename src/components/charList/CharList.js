6bedb7040f34a4734a0f69ba6873e974f463d72e
Yurii <paraska108@gmail.com>
2022-09-16 12:21:46 +0300

    add style chose character

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -58,15 +58,39 @@ class CharList extends Component {
         });
     }
 
+    itemRefs = [];
+    
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+        console.log(this.itemRefs)
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             const http = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
             let objectFit = item.thumbnail === http ? { 'objectFit': 'fill' } : null;
 
             return (
                 <li className="char__item"
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}>
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}
                     <img src={item.thumbnail} alt={item.name} style={objectFit} />
                     <div className="char__name">{item.name}</div>
                 </li>
