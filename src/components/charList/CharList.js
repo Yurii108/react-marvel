73e18570964fe60ca957cc6f99648edd4cf5f355
Yurii <paraska108@gmail.com>
2022-09-20 11:00:22 +0300

    make it better

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import MarvelService from '../../services/MarvelServices.js';
+import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
